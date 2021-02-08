104c387f395700cdf80c5513fc10d96c5374fbe2
Su Kai <sukai3316@gmail.com>
2021-02-08 18:08:27 +0900

    improve labeling process

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -500,7 +500,7 @@
             this.button19.Name = "button19";
             this.button19.Size = new System.Drawing.Size(110, 63);
             this.button19.TabIndex = 29;
-            this.button19.Text = "Label History";
+            this.button19.Text = "Label History Mode";
             this.button19.UseVisualStyleBackColor = true;
             this.button19.Click += new System.EventHandler(this.button19_Click_1);
             // 
